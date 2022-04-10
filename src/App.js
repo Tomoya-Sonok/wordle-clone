3abb9f53f93d5854f6763bc12c35aceb9e513f6a
Sonokui <t.sonokui@la-haina.jp>
2022-04-10 09:06:45 +0900

    first commit/ initial setup

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import "./App.css"
+import Board from "./components/Board"
+import Keyboard from "./components/Keyboard"
+import { boardDefault } from "./Words"
+import { createContext, useState } from "react"
+
+// Context API を使用
+// 以下のAppContext.Providerに入れたコンポーネント内で
+// valueとして渡した変数や関数が使えるようになる
+export const AppContext = createContext()
+
+function App() {
+  const [board, setBoard] = useState(boardDefault)
+  return (
+    <div className="App">
+      <nav>
+        <h1>Worlde</h1>
+      </nav>
+      <AppContext.Provider value={{ board, setBoard }}>
+        <div className="game">
+          <Board />
+          <Keyboard />
+        </div>
+      </AppContext.Provider>
+    </div>
+  )
+}
+
+export default App
